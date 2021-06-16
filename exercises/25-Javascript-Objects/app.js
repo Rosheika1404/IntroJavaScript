39bc2eab8fb54d0db42306e00f186bb935ad6057
Sheika1404 <rosheika1404@gmail.com>
2021-06-16 23:09:15 +0000

    Exercise 25

--- exercises/25-Javascript-Objects/app.js
+++ exercises/25-Javascript-Objects/app.js
@@ -3,7 +3,7 @@ var person = {
     lastname: "Doe",
     age: 35,                     //Number
     gender: "male",
-    lucky_numbers: [ 7, 11, 13, 17], //Array
+    lucky_numbers: [7, 11, 13, 17], //Array
     significant_other: person2   //Object, yes the same variable/object defined after
 };
 
@@ -12,7 +12,7 @@ var person2 = {
     lastname: "Doe",
     age: 38,
     gender: "female",
-    lucky_numbers: [ 2, 4, 6, 8],
+    lucky_numbers: [2, 4, 6, 8],
     significant_other: person
 };
 
@@ -22,16 +22,49 @@ var family = {
 };
 
 
-function addAllFamilyLuckyNumbers(anArray){
-  var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
+function addAllFamilyLuckyNumbers(anArray) {
+    var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
 
-  //To-Do: loop and add; consider nested loops
-  //Hint: use the anArray variable to get all of the lucky numbers
-  
-  return sumOfAllLuckyNumbers;
+    //To-Do: loop and add; consider nested loops
+    //Hint: use the anArray variable to get all of the lucky numbers
+    // for (var i = 0, l = person.lucky_numbers.length; i < l; i++) {
+    //     for (var j = 0, m = person2.lucky_numbers.length; j < m; j++) {
+    //         for (var k = 0, n = person3.lucky_numbers.length; k < n; k++) {
+
+
+    //         }
+    //     }
+    // }
+    for (let i = 0; i < person.lucky_numbers.length; i++) {
+        sumOfAllLuckyNumbers = sumOfAllLuckyNumbers + person.lucky_numbers[i]
+    }
+    for (let i = 0; i < person2.lucky_numbers.length; i++) {
+        sumOfAllLuckyNumbers = sumOfAllLuckyNumbers + person2.lucky_numbers[i]
+    }
+    for (let i = 0; i < person3.lucky_numbers.length; i++) {
+        sumOfAllLuckyNumbers = sumOfAllLuckyNumbers + person3.lucky_numbers[i]
+    }
+
+
+    return sumOfAllLuckyNumbers;
 }
 
 //Enter all your code here:
+person.lucky_numbers[3] = 33;
+
+var person3 = {
+    name: "Jimmy",
+    lastname: "Doe",
+    age: 13,
+    gender: "male",
+    lucky_numbers: [1, 2, 3, 4],
+    significant_other: null
+};
+
+family.members.push('Jimmy');
+
+
+
 
 
 //Do not make changes below:
